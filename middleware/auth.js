f21c6c1e9eb9c188f3e720f6cc60e9938b75d417
huycuongngo <cuong.nh@softenmind.com>
2024-07-02 18:42:58 +0700

    feat: update api auth

---
+++ middleware/auth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken'
+import { handleResponseError } from '../utils/responses.js'
+
+export const auth = (req, res, next) => {
+  const { authorization } = req.headers
+  if (!authorization) {
+    handleResponseError(res, 401, 'Invalid authorization')
+    return
+  }
+  const accessToken = authorization.split(' ')[1]
+  if (!accessToken) {
+    handleResponseError(res, 401, 'Invalid access token')
+    return
+  }
+  const { user } = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET_KEY)
+  console.log("auth middleware", user)
+  if (!user) {
+    handleResponseError(res, 401, 'Invalid access token')
+    return
+  }
+  next()
+}
+
+export const authAdmin = (req, res, next) => {
+  const { authorization } = req.headers
+  const accessToken = authorization.split(' ')[1]
+  const { user } = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET_KEY)
+  if (user.role !== "admin") {
+    handleResponseError(res, 404, 'Forbidden')
+    return
+  }
+  next()
+}
---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { register, login, logout } from '../controllers/authController.js'
+import { auth } from '../middleware/auth.js'
+
+const router = express.Router();
+
+router.post('/register', register)
+router.post('/login', login)
+router.post('/logout', auth, logout)
+
+export { router as userRoutes }
