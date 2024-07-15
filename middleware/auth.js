c589c6f07a97389a3cb31726c82ef251d4c2afa9
phuochaufms71 <phuochaubmw@gmail.com>
2024-07-16 05:01:15 +0700

    feat: done project

--- middleware/auth.js
+++ middleware/auth.js
@@ -24,7 +24,7 @@ export const auth = (req, res, next) => {
 
 export const authAdmin = (req, res, next) => {
     const { authorization } = req.headers;
-    const accessToken = authorization.split(" ")[1];
+    const accessToken = authorization.split(' ')[1];
     const { user } = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET_KEY);
     if (user.role !== "admin") {
         handleResponseError(res, 403, "Forbidden")
