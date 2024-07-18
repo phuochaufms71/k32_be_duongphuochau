7598c718d77e9f31255cac258281dd28a2a46098
phuochaufms71 <phuochaubmw@gmail.com>
2024-07-18 15:19:00 +0700

    feat:done project

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { register, login, logout } from '../controller/authController.js';
-import { auth } from '../middleware/auth.js';
 
 const router = express.Router();
 
