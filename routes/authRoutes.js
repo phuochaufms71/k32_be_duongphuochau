9c18e2a88ec332bbd6529579937719cfaa8b5ed4
phuochaufms71 <phuochaubmw@gmail.com>
2024-07-18 15:15:32 +0700

    feat:done project

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', auth, logout);
+router.post('/logout', logout);
 
 export { router as authRoutes }
